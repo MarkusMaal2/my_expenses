6e96834d452edd78df0e789df2558d24e072eb35
MarkusMaal2 <markus.maal@voco.ee>
2023-01-10 11:22:33 +0200

    Pass data view props to first custom component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg'
 import './App.css'
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ExpenseItem />
-    </div>
-  )
+    const expenses = [
+        {
+            date: new Date(2023, 0, 10),
+            title: "New book",
+            price: 29.99
+        },
+        {
+            date: new Date(2022, 6, 23),
+            title: "Ice cream",
+            price: 1.99
+        }
+    ]
+    return (
+        <div className="App">
+          <ExpenseItem expenseData={expenses[0]} />
+          <ExpenseItem expenseData={expenses[1]}/>
+        </div>
+    )
 }
 
 export default App
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
         <div className="expense-item">
             <div>
-                Date
+                {props.expenseData.date.toString()}
             </div>
             <div className="expense-item__description">
                 <h2>
-                    Title
+                    {props.expenseData.title}
                 </h2>
                 <div className="expense-item__price">
-                    Amount
+                    {props.expenseData.price}€
                 </div>
             </div>
         </div>
