f89f52a85b10bd9602920e9495717303080b39a3
MarkusMaal2 <markus.maal@voco.ee>
2023-01-18 15:35:23 +0200

    Create a submitHandler and clear input fields after the data is submitted

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,20 +17,34 @@ const ExpenseForm = (props) => {
     const dateChangeHandler = (e) => {
         setEnteredDate(e.target.value)
     }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        //console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredDate('')
+        setEnteredAmount('')
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler}/>
+                    <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler} value={enteredDate}/>
                 </div>
             </div>
             <div className="new-expense__actions">
