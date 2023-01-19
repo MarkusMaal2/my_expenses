27ebf9a1bc1f3df1125716d48f1a80483d444d93
MarkusMaal2 <markus.maal@voco.ee>
2023-01-19 11:01:17 +0200

    Add data to expenses array using setExpense in addExpenseHandler

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import './App.css'
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, {useState} from "react";
+
+// sample data, just for testing
+const DUMMY_EXPENSES = [
+    {
+        date: new Date(2023, 0, 10),
+        title: "New book",
+        price: 29.99
+    },
+    {
+        date: new Date(2022, 6, 23),
+        title: "Ice cream",
+        price: 1.99
+    },
+    {
+        date: new Date(2023, 9, 31),
+        title: "Shoes",
+        price: 14.55
+    }
+]
 
 const App = () => {
-    // sample data, just for testing
-    const DUMMY_EXPENSES = [
-        {
-            date: new Date(2023, 0, 10),
-            title: "New book",
-            price: 29.99
-        },
-        {
-            date: new Date(2022, 6, 23),
-            title: "Ice cream",
-            price: 1.99
-        },
-        {
-            date: new Date(2023, 9, 31),
-            title: "Shoes",
-            price: 14.55
-        }
-    ]
-
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
     const addExpenseHandler = (expense) => {
         console.log("Recieved in App.js")
-        console.log(expense)
+        setExpenses((previousExpenses) => {
+            return [expense, ...expenses]
+        })
     }
+    console.log(expenses)
     return (
         <div className="App">
             <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
