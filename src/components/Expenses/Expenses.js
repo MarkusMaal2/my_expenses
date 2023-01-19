eff221ed928fbd7c6e1088c987caff6ef5c98feb
MarkusMaal2 <markus.maal@voco.ee>
2023-01-19 13:00:29 +0200

    Refactor Expenses.js and display a message when no expenses are found for the specific year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,19 +9,29 @@ function Expenses(props) {
         console.log(expense)
     })*/
     const [currentYear, setCurrentYear] = useState(props.currentYear)
+    // default value
+    let expensesContent = <p>No expenses found</p>
+    // create an array of filtered expenses
+    const filteredExpenses = props.expenses.filter((item) => {
+        return item.date.getFullYear() === Number(currentYear)
+    })
+    // if filtered expenses contains something, map each ExpenseItem component to an expense from the list
+    if (filteredExpenses.length > 0)
+    {
+        expensesContent = filteredExpenses.map((expense) => {
+            return <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}></ExpenseItem>
+        })
+    }
+    // display result
     return (
         <Card className="expenses">
             <ExpensesFilter onModifyExpenseFilter={(year) => { setCurrentYear(year); props.onYearChange(currentYear);}}></ExpensesFilter>
             {
-                props.expenses.filter((item) => {
-                    return item.date.getFullYear() === Number(currentYear)
-                }).map((expense) => {
-                    return <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}></ExpenseItem>
-                })
+                expensesContent
             }
         </Card>
     )
