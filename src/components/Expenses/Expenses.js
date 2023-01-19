d28a9bedc05c2c5d0008a8f0608012b88b48d3fe
MarkusMaal2 <markus.maal@voco.ee>
2023-01-19 10:34:19 +0200

    Add ExpensesFilter component and move data bottom-up to Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 function Expenses(props) {
+    let currentYear = 2023
+
+    const modifyExpenseFilterHandler = (year) => {
+        console.log(`Year in Expenses.js: ${year}`)
+        currentYear = year
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onModifyExpenseFilter={modifyExpenseFilterHandler}></ExpensesFilter>
             <ExpenseItem expenseData={props.expenses[0]} />
             <ExpenseItem expenseData={props.expenses[1]}/>
         </Card>
